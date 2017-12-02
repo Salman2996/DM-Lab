046481d7b5aba77d727757462b12239cc09da0f6
Salman2996 <smr.2996@gmail.com>
2017-12-02 15:26:18 +0530

    2nd Lab internal

---
+++ R/4/4.r
@@ -0,0 +1,39 @@
+# install.packages("caret", dependencies = TRUE)
+# install.packages("klaR", dependencies = TRUE)
+
+# Naive Bayes classifier
+library("caret")
+library("klaR")
+# use the iris dataset
+data(iris)
+# display first few entries
+head(iris)
+
+# split data into train(67%) and test(33%) set
+split = 0.67
+trainIndex <- createDataPartition(iris$Species, p = split, list = FALSE)
+trainData <- iris[trainIndex, ]
+testData <- iris[-trainIndex, ]
+
+# train using naive bayes
+model <- NaiveBayes(Species~., data = trainData)
+
+#make predictions
+x_test <- testData[, 1:4]
+y_test <- testData[, 5]
+predictions <- predict(model, x_test)
+
+# display confusion matrix with accuracy
+confusionMatrix(predictions$class, y_test)
+
+plot(model)
+
+# Decision Tree
+library("party")
+# construct decision tree
+irisTree <- ctree(Species~., data = iris)
+# print the tree summary
+print(irisTree)
+#plot the tree
+plot(irisTree)
+plot(irisTree, type = "simple")
