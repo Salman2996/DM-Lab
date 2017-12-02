086c49fb70f788e8ca8fc1e6b970de84014c4e6c
Salman2996 <smr.2996@gmail.com>
2017-12-02 16:08:43 +0530

    2nd Lab Internals

--- R/3/3.r
+++ R/3/3.r
@@ -7,6 +7,6 @@ x <- read.transactions("3.csv", format = "basket", sep = ",", cols = NULL)
 # generate the rules using apriori algorithm
 rules <- apriori(x,parameter = list(supp = 0.3, conf = 0.5, target = "rules"))
 
-# display and plot the generatee d rules
+# display and plot the generatee d rules, visualize
 inspect(rules)
 plot(rules)
